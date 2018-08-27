131fe3d622aa765a044fede9d38c9b3fbcd26966
zhuwenqian <zhuwenqian@b.360.cn>
2018-08-27 19:07:47 +0800

    add heap sort

--- leetcode/heap_sort.py
+++ leetcode/heap_sort.py
@@ -3,60 +3,54 @@
 当前节点 i
 其父节点 (i - 1) // 2
 子节点   (2 * i + 1), (2 * i + 2)
+
+求最小的k个数要用大顶堆
 """
 
 
-def heap_sort_asc(l):
-    def max_heapify(root_index, end_index):
-        max_child_index = root_index * 2 + 1
-        if max_child_index + 1 <= end_index:
-            if l[max_child_index + 1] > l[max_child_index]:
-                max_child_index += 1
-        if l[max_child_index] > l[root_index]:
-            l[max_child_index], l[root_index] = l[root_index], l[max_child_index]
-
-    for end_index in range(len(l) - 1, 0, -1):    # 当前堆的末尾元素下标
-        max_root_index = (end_index - 1) // 2     # 最大父节点下标
-        for root_index in range(max_root_index, -1, -1):  # 从大到小调整所有非叶子节点
-            max_heapify(root_index, end_index)
-        l[0], l[end_index] = l[end_index], l[0]
+def fix_down(l, root, heap_len):
+    cur = root
+    child = 2 * cur + 1
+    while child < heap_len:
+        if child + 1 < heap_len and l[child + 1] > l[child]:
+            child = child + 1
+        if l[cur] < l[child]:
+            l[cur], l[child] = l[child], l[cur]
+            cur, child = child, 2 * child + 1
+        else:
+            break
 
 
-def heap_sort_desc(l):
-    def max_heapify(root_index, end_index):
-        max_child_index = root_index * 2 + 1
-        if max_child_index + 1 <= end_index:
-            if l[max_child_index + 1] < l[max_child_index]:
-                max_child_index += 1
-        if l[max_child_index] < l[root_index]:
-            l[max_child_index], l[root_index] = l[root_index], l[max_child_index]
-
-    for end_index in range(len(l) - 1, 0, -1):
-        max_root_index = (end_index - 1) // 2
-        for root_index in range(max_root_index, -1, -1):
-            max_heapify(root_index, end_index)
-        l[0], l[end_index] = l[end_index], l[0]
+def create_heap(l, s, e):
+    heap_len = e - s + 1
+    max_non_leaf_node = (heap_len - 1) // 2
+    for i in range(max_non_leaf_node, s - 1, -1):
+        fix_down(l, i, heap_len)
 
 
-def find_kth_min(l, k):
-    def heap_min(r, e):
-        child = 2 * r + 1
-        if child + 1 <= e and l[child + 1] < l[child]:
-            child += 1
-        if l[child] < l[r]:
-            l[r], l[child] = l[child], l[r]
+def heap_sort(l):
+    create_heap(l, 0, len(l) - 1)
+    heap_len = len(l)
+    while heap_len > 1:
+        l[heap_len - 1], l[0] = l[0], l[heap_len - 1]
+        heap_len -= 1
+        fix_down(l, 0, heap_len)
 
-    for e in range(len(l) - 1, len(l) - k - 1, -1):
-        max_r = (e - 1) // 2
-        for r in range(max_r, -1, -1):
-            heap_min(r, e)
-        l[e], l[0] = l[0], l[e]
-    return l[-k]
+
+def top_k_min(l, k):
+    create_heap(l, 0, k - 1)
+    for i in range(k, len(l)):
+        if l[i] < l[0]:
+            l[0], l[i] = l[i], l[0]
+            fix_down(l, 0, k)
+    return l[0:k]
 
 
 if __name__ == "__main__":
-    l = [6, 5, 2, 8, 9, 7, 7, 3, 1, 9, 0]
+    l = [9, 8, 3, 2, 4, 5, 6, 8, 0, 1, 3]
     print(l)
-    r = find_kth_min(l, 5)
-    print r
+    heap_sort(l)
     print(l)
+    l = [9, 8, 3, 2, 4, 5, 6, 8, 0, 1, 3]
+    res = top_k_min(l, 3)
+    print res
